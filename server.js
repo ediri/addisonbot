0eac9aa5bba5694a735fc50b6d6ea00399c522be
Engin.Diri <engin.diri@addison.de>
2016-05-12 20:45:28 +0200

    initial

--- server.js
+++ server.js
@@ -115,6 +115,7 @@ function createPayment(userId) {
                 uri: invoiceEndPoint,
                 json: paypal
             }, function (error, response, body) {
+                console.log(body);
                 if (response.statusCode === 200) {
                     var json = JSON.parse(body);
                     console.log(json);
