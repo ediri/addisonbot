717e61051da0aa873e3bf54185a33e8bce436a4c
Engin.Diri <engin.diri@addison.de>
2016-05-12 22:24:57 +0200

    initial

--- server.js
+++ server.js
@@ -35,14 +35,11 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-
     if (req.query.payment === 'ok') {
         setPaymentDone(req, res);
     } else {
         getPaymentDetails(req, res);
     }
-
-
 });
 
 
@@ -87,8 +84,6 @@ function setPaymentDone(req, res) {
         }
     }, function (error, response, body) {
         if (response.statusCode === 200) {
-            var json = JSON.parse(body);
-            console.log(json);
             //todo
             res.send('ok');
         }
