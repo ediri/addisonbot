d6cb99e4d59ec622acdca4a3bad97e2aa21bb4e5
Engin.Diri <engin.diri@addison.de>
2016-05-12 12:06:16 +0200

    initial

--- server.js
+++ server.js
@@ -5,83 +5,28 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var request = require('request');
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
 var app = express();
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-// der Order views wird geladen. Hier liegen alle JADE Templates drin
 app.set('views', path.join(__dirname, 'views'));
-
-// JADE Template Engine verwenden! Details -> http://jade-lang.com/reference/
 app.set('view engine', 'pug');
 
 var port = 8080;
 
 
-passport.use(new Strategy({
-        clientID: "1147997221899426",
-        clientSecret: "fd96c6a7258691eb0a4347e5069ddf1a",
-        callbackURL: 'https://lit-falls-27030.herokuapp.com/login/facebook/return'
-    },
-    function (accessToken, refreshToken, profile, cb) {
-        console.log(profile)
-        console.log(accessToken);
-        return cb(null, profile);
-    })
-);
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.get('/', function (req, res) {
-    console.log(req.user);
-    res.render('home', {
-        user: req.user,
-    });
+    res.render('home');
 });
 
 app.post('/', function (req, res) {
-    console.log(req.user);
-    res.render('home', {
-        user: req.user,
-    });
+    res.render('home');
 });
 
-app.get('/login', function (req, res) {
-        res.render('login');
-});
-app.get('/login/facebook',
-    passport.authenticate('facebook'));
-
-app.get('/login/facebook/return',
-    passport.authenticate('facebook', {successRedirect:'/',failureRedirect: '/login'}));
-/*
-
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function (req, res) {
-        res.render('profile', {user: req.user});
-    }
-);
-
-app.post('/', function (req, res) {
-    res.send('Hello From NodeJS ' + req.query.name + ' !')
-});
 
 app.post('/webhook', function (req, res) {
-    console.log(req.body);
     getFriendsList();
     var events = req.body.entry[0].messaging;
 
@@ -107,10 +52,11 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-function getFriendsList() {
+function getFriendsList(id) {
     request({
         method: 'GET',
-        uri: "https://graph.facebook.com/v2.6/me/invitable_friends?access_token=EAAQUGO6dTKIBAK3YxLdBzngDR8KlgDKjR4Vi1ySz5kW4SlW1eY3xawU8oT1aZA2xyJBPhYVynaWkHtE0YS95rA3B3mx4EJwaYcT66O0BfgxbuLkZADxorxsb9YuP3HZABNFLQ759N0UpktoAHL6xNCndZC3ALFRfmZCaiP1g1NoMzFGM8pbC7&debug=all&format=json&method=get&pretty=0&suppress_http_code=1",
+        uri: "https://graph.facebook.com/v2.6/" + id + "/friends",
+        headers: {"authorization": "Bearer 1147997221899426|fd96c6a7258691eb0a4347e5069ddf1a"}
     }, function (error, response, body) {
         if (error) {
             console.log('Error sending message: ', error);
@@ -166,6 +112,6 @@ function sendMessage(recipientId, message) {
         }
     });
 }
-*/
+
 app.listen(process.env.PORT || port);
 console.log('Running on http://localhost:' + port);
